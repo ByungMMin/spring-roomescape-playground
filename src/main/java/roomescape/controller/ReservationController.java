1f530853be5a4443c0aec9192532fd609def123e
BBmin <kbm3751@soongsil.ac.kr>
2024-07-27 22:32:39 +0900

    feat: 예약 추가 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,17 +1,22 @@
 package roomescape.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Controller
 public class ReservationController {
 
-    private List<Reservation> reservations = new ArrayList<>();
+    private final List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
 
     // 예약 관리 페이지 응답
     @GetMapping("/reservation")
@@ -25,4 +30,23 @@ public class ReservationController {
         return ResponseEntity.ok().body(reservations);
     }
 
+    // 예약 추가 API
+    @PostMapping("/reservations/reservation")
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {
+        reservation.setId(index.getAndIncrement()); // 예약에 ID를 부여합니다.
+        reservations.add(reservation); // 예약을 목록에 추가합니다.
+        // 예약이 추가된 후, 201 Created 상태와 함께 예약 객체를 반환합니다.
+        return ResponseEntity.status(HttpStatus.CREATED).body(reservation);
+    }
+
+    // 예약 삭제 API
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        boolean removed = reservations.removeIf(reservation -> reservation.getId().equals(id));
+        if (removed) {
+            return ResponseEntity.noContent().build(); // 204 No Content 상태 반환
+        } else {
+            return ResponseEntity.notFound().build(); // 404 Not Found 상태 반환
+        }
+    }
 }
--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,7 +1,5 @@
 package roomescape.domain;
 
-import java.time.LocalDateTime;
-
 public class Reservation {
     private Long id;
     private String name;
@@ -24,6 +22,8 @@ public class Reservation {
     public Long getId() {
         return id;
     }
+
+    public void setId(Long id) { this.id = id; }
 
     public String getName() {
         return name;
